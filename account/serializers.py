398dd6da750ad62d0917084cb0ef82c9ec10223d
jforv <xbrain1@gmail.com>
2018-03-13 14:11:13 -0400

    Before starting next chapter

--- account/serializers.py
+++ account/serializers.py
@@ -2,15 +2,22 @@ from rest_framework import serializers
 from account.models import Account
 
 class AccountSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    accountType = serializers.CharField(max_length=24)
-
-    def create(self, validated_data):
-        return Account.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.accountType = validated_data.get('accountType', instance.accountType)
-        instance.save()
-        return instance
+    class Meta:
+        model = Account
+        fields = ('id',
+                'name',
+                'accountType',
+        )
+    # pk = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(max_length=100)
+    # accountType = serializers.CharField(max_length=24)
+
+    # def create(self, validated_data):
+    #     return Account.objects.create(**validated_data)
+    
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.accountType = validated_data.get('accountType', instance.accountType)
+    #     instance.save()
+    #     return instance
--- lebilan/settings.py
+++ lebilan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+oxg0zq8ceb!ij*-=to19&64(4nw)xve&am5b(qb8=5bhv7$b!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
